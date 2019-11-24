3a0139b2e3c555126b2ac1fa541e0d1d20154e18
Vadim Lyubanenko <lubanenko@yandex.ru>
2019-11-24 23:28:07 +0300

    add DataClusteringSample1821.tar.gz

--- download.sh
+++ download.sh
@@ -9,15 +9,14 @@ wget -O cat_detect.ftz https://www.dropbox.com/s/3xwj6rxsk6eykcr/cat_detect.ftz
 cd ..
 
 # Datasets
+
+data_list=(
+    "https://data-static.usercontent.dev/DataClusteringSample0107.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringSample0817.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringSample1821.tar.gz"
+)
+
 mkdir data
-cd data
-
-wget https://data-static.usercontent.dev/DataClusteringSample0107.tar.gz
-tar -xzvf DataClusteringSample0107.tar.gz
-rm DataClusteringSample0107.tar.gz
-
-wget https://data-static.usercontent.dev/DataClusteringSample0817.tar.gz
-tar -xzvf DataClusteringSample0817.tar.gz
-rm DataClusteringSample0817.tar.gz
-
-cd ..
+for url in ${data_list[@]}; do
+    wget -qO - $url | tar -xvz -C data
+done

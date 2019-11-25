9d45fbf6dda02f9575e229364a16fc171ead9be4
Ilya Gusev <phoenixilya@gmail.com>
2019-11-25 17:58:08 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Models
-mkdir models
-cd models
-wget -O lang_detect.ftz https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.ftz
-wget -O news_detect.ftz https://www.dropbox.com/s/squ9rnm6a4ixkza/news_detect.ftz
-wget -O cat_detect.ftz https://www.dropbox.com/s/3xwj6rxsk6eykcr/cat_detect.ftz
-cd ..
-
 # Datasets
 
 data_list=(

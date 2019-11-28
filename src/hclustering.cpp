dc89e741f236fbd1905c087b35003b4ec1ce90ac
Ilya Gusev <phoenixilya@gmail.com>
2019-11-28 18:16:29 +0300

    n^2 clustering

--- src/hclustering.cpp
+++ src/hclustering.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 #include "hclustering.h"
 
@@ -6,43 +7,68 @@ HierarchicalClustering::HierarchicalClustering(float distanceThreshold)
     : DistanceThreshold(distanceThreshold)
 {}
 
+// SLINK: https://sites.cs.ucsb.edu/~veronika/MAE/summary_SLINK_Sibson72.pdf
 void HierarchicalClustering::Cluster(const Eigen::MatrixXf& points) {
     Eigen::MatrixXf distances(points.rows(), points.rows());
     fillDistanceMatrix(points, distances);
+
+    // Prepare 3 arrays
+    const float INF_DISTANCE = 1.0f;
     size_t n = points.rows();
     Labels = std::vector<size_t>(n);
     for (size_t i = 0; i < n; i++) {
         Labels[i] = i;
     }
+    std::vector<size_t> nn(n);
+    std::vector<float> nnDistances(n);
+    for (size_t i = 0; i < n; i++) {
+        Eigen::Index minJ;
+        nnDistances[i] = distances.row(i).minCoeff(&minJ);
+        nn[i] = minJ;
+    }
+
+    // Main linking loop
     size_t level = 0;
     while (level < n - 1) {
-        Eigen::Index minI, minJ;
-        float minDistance = distances.minCoeff(&minI, &minJ);
+        // Calculate minimal distance
+        auto minDistanceIt = std::min_element(nnDistances.begin(), nnDistances.end());
+        size_t minI = std::distance(nnDistances.begin(), minDistanceIt);
+        size_t minJ = nn[minI];
+        float minDistance = *minDistanceIt;
         if (minDistance > DistanceThreshold) {
             break;
         }
+
+        // Link minJ to minI
         for (size_t i = 0; i < n; i++) {
             if (Labels[i] == minJ) {
                 Labels[i] = minI;
             }
         }
-        for (size_t j = 0; j < distances.cols(); j++) {
-            if (j == minJ) {
+
+        // Update distance matrix and nearest neighbors
+        nnDistances[minI] = INF_DISTANCE;
+        for (size_t k = 0; k < static_cast<size_t>(distances.rows()); k++) {
+            if (k == minI || k == minJ) {
                 continue;
             }
-            distances(minI, j) = std::min(distances(minJ, j), distances(minI, j));
-            distances(j, minI) = distances(minI, j);
+            float newDistance = std::min(distances(minJ, k), distances(minI, k));
+            distances(minI, k) = newDistance;
+            distances(k, minI) = newDistance;
+            if (newDistance < nnDistances[minI]) {
+                nnDistances[minI] = newDistance;
+                nn[minI] = k;
+            }
         }
-        for (size_t i = 0; i < distances.cols(); i++) {
-            distances(minJ, i) = 1.0f;
-            distances(i, minJ) = 1.0f;
+
+        // Remove minJ row and column from distance matrix
+        nnDistances[minJ] = INF_DISTANCE;
+        for (size_t i = 0; i < static_cast<size_t>(distances.rows()); i++) {
+            distances(minJ, i) = INF_DISTANCE;
+            distances(i, minJ) = INF_DISTANCE;
         }
         level += 1;
     }
-    for (size_t i = 0; i < n; i++) {
-        std::cerr << Labels[i] << " ";
-    }
-    std::cerr << std::endl;
 }
 
 const std::vector<size_t>& HierarchicalClustering::GetLabels() const {
@@ -50,6 +76,8 @@ const std::vector<size_t>& HierarchicalClustering::GetLabels() const {
 }
 
 void HierarchicalClustering::fillDistanceMatrix(const Eigen::MatrixXf& points, Eigen::MatrixXf& distances) {
+    // Assuming points are on unit sphere
+    // Normalize to [0.0, 1.0]
     distances = -((points * points.transpose()).array() + 1.0f) / 2.0f + 1.0f;
     distances += distances.Identity(distances.rows(), distances.cols());
 }

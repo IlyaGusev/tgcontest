cb99263e766849b5cc96c9710cf1ca4e6f435356
Ilya Gusev <phoenixilya@gmail.com>
2019-12-01 11:58:05 +0300

    json_to_toloka with translations

--- scripts/json_to_toloka.py
+++ scripts/json_to_toloka.py
@@ -1,25 +1,36 @@
 import argparse
 import sys
+import time
 import json
 import csv
 import random
 import os
+from yandex.Translater import Translater
 
 random.seed(42)
 
 
 def clean_text(text):
-    return text.replace("\t", " ").replace("\n", " ")
+    return text.replace("\t", " ").replace("\n", " ").replace('"', "'")
 
 
-def main(json_file_name, output_dir, honey_file_name, pool_size, honey_size, existing_markup_file):
+def main(json_file_name, output_dir, honey_file_name,
+         pool_size, honey_size, existing_markup_file,
+         use_translation, skip, tr_key):
     honey = []
     with open(honey_file_name, "r") as r:
         header = tuple(next(r).strip().split("\t"))
-        assert header == ("INPUT:url", "INPUT:title", "INPUT:text", "GOLDEN:res")
+        if not use_translation:
+            assert header == ("INPUT:url", "INPUT:title", "INPUT:text", "GOLDEN:res")
+        else:
+            assert header == ("INPUT:url", "INPUT:title", "INPUT:text", "GOLDEN:res", "INPUT:trans_title", "INPUT:trans_text")
         for line in r:
-            url, title, text, res = line.strip().split("\t")
-            honey.append({"url": url, "title": title, "text": text, "res": res})
+            if not use_translation:
+                url, title, text, res = line.strip().split("\t")
+                honey.append({"url": url, "title": title, "text": text, "res": res})
+            else:
+                url, title, text, res, trans_title, trans_text = line.strip().split("\t")
+                honey.append({"url": url, "title": title, "text": text, "res": res, "trans_title": trans_title, "trans_text": trans_text})
 
     existing_urls = set()
     if existing_markup_file:
@@ -30,19 +41,28 @@ def main(json_file_name, output_dir, honey_file_name, pool_size, honey_size, exi
                 url = line.strip().split("\t")[0]
                 existing_urls.add(url)
 
+    tr = Translater()
+    tr.set_key(tr_key)
+    tr.set_from_lang("en")
+    tr.set_to_lang("ru")
+
     with open(json_file_name, "r") as r:
         records = json.load(r)
         random.shuffle(records)
         current_pool = []
         i = 0
-        for record in records:
+        for record in records[skip:]:
             if i % pool_size == 0 and i != 0:
                 clean_pool = []
                 for r in current_pool:
                     title = clean_text(r["title"])
                     text = clean_text(r["text"])
                     url = r["url"]
-                    clean_pool.append({"url": url, "title": title, "text": text, "res": ""})
+                    pool_record = {"url": url, "title": title, "text": text, "res": ""}
+                    if use_translation:
+                        pool_record["trans_title"] = r["trans_title"]
+                        pool_record["trans_text"] = r["trans_text"]
+                    clean_pool.append(pool_record)
                 random.shuffle(honey)
                 current_pool = clean_pool + honey[:honey_size]
                 random.shuffle(current_pool)
@@ -51,14 +71,31 @@ def main(json_file_name, output_dir, honey_file_name, pool_size, honey_size, exi
                 pool_file_name = "pool_{}.tsv".format(pool_num)
                 pool_file_name = os.path.join(output_dir, pool_file_name)
                 with open(pool_file_name, "w", encoding="utf-8") as w:
-                    w.write("INPUT:url\tINPUT:title\tINPUT:text\tGOLDEN:res\n")
+                    header = "INPUT:url\tINPUT:title\tINPUT:text\tGOLDEN:res";
+                    if use_translation:
+                        header += "\tINPUT:trans_title\tINPUT:trans_text";
+                    w.write(header + "\n")
                     writer = csv.writer(w, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
                     for r in current_pool:
-                        writer.writerow((r["url"], r["title"], r["text"], r["res"]))
+                        row = [r["url"], r["title"], r["text"], r["res"]]
+                        if use_translation:
+                            row += [r["trans_title"], r["trans_text"]]
+                        writer.writerow(tuple(row))
                 current_pool = []
             if record["url"] in existing_urls or not clean_text(record["text"]) or not clean_text(record["title"]):
                 print("Skipping: {}".format(record["url"]))
                 continue
+            if use_translation:
+                try:
+                    tr.set_text(clean_text(record["title"]))
+                    record["trans_title"] = tr.translate()
+                    tr.set_text(clean_text(record["text"]))
+                    record["trans_text"] = tr.translate()
+                except Exception as e:
+                    print("Translation failed: {}".format(e))
+                    continue
+
+            print("OK")
             current_pool.append(record)
             i += 1
 
@@ -71,6 +108,9 @@ if __name__ == "__main__":
     parser.add_argument("--pool-size", type=int, default=200)
     parser.add_argument("--honey-size", type=int, default=50)
     parser.add_argument("--existing-markup-file", type=str, default=None)
+    parser.add_argument("--use-translation", default=False, action="store_true")
+    parser.add_argument("--skip", default=0, type=int)
+    parser.add_argument("--tr-key", type=str, default=None)
     args = parser.parse_args()
     main(**vars(args))
 

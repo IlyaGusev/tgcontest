d941a2b2397d1173bb14e52180a250025f60c5d2
Ilya Gusev <phoenixilya@gmail.com>
2019-12-01 17:17:17 +0300

    Sigmoids

--- src/util.h
+++ src/util.h
@@ -6,3 +6,5 @@
 void ReadFileNames(const std::string& directory, std::vector<std::string>& fileNames, int nDocs=-1);
 std::string GetHost(const std::string&);
 std::string GetCleanFileName(const std::string& fileName);
+float Sigmoid(float x);
+double Sigmoid(double x);

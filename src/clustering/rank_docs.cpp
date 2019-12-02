73acfca676f0580ef3ae5af4c2650b06cc7934e6
yandexgivemeofferplz <tashkov95@ya.ru>
2019-12-02 19:22:21 +0300

    use merged ru en pr rating

--- src/clustering/rank_docs.cpp
+++ src/clustering/rank_docs.cpp
@@ -5,22 +5,20 @@
 #include "clustering.h"
 
 
-std::unordered_map<std::string, double> LoadRatings(const std::vector<std::string>& ratingFiles) {
+std::unordered_map<std::string, double> LoadRatings(const std::string& ratingPath) {
     std::unordered_map<std::string, double> output;
     std::string line;
 
-    for (const auto& ratingPath : ratingFiles) {
-        std::ifstream rating(ratingPath);
-        if (rating.is_open()) {
-            while (std::getline(rating, line)) {
-                std::vector<std::string> lineSplitted;
-                boost::split(lineSplitted, line, boost::is_any_of("\t"));
+    std::ifstream rating(ratingPath);
+    if (rating.is_open()) {
+        while (std::getline(rating, line)) {
+            std::vector<std::string> lineSplitted;
+            boost::split(lineSplitted, line, boost::is_any_of("\t"));
 
-                output[lineSplitted[1]] = std::stod(lineSplitted[0]);
-            }
-        } else {
-            std::cerr << "rating file is not available" << std::endl;
+            output[lineSplitted[1]] = std::stod(lineSplitted[0]);
         }
+    } else {
+        std::cerr << "rating file is not available" << std::endl;
     }
     return output;
 }

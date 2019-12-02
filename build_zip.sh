d002d6aac1c8e221e888d5e41127aac0e5a08b5d
Ilya Gusev <phoenixilya@gmail.com>
2019-12-02 16:57:27 +0300

    Build submission script

---
+++ build_zip.sh
@@ -0,0 +1,26 @@
+mkdir temp
+cd temp
+cp -R ../src src
+cp -R ../models models
+cp -R ../ratings ratings
+cp -R ../scripts scripts
+cp -R ../thirdparty thirdparty
+rm -rf thirdparty/fasttext/.git
+rm -rf thirdparty/eigen/.git
+cp ../LICENSE LICENSE
+cp ../NOTICE NOTICE
+cp ../README.md README.md
+cp ../CMakeLists.txt CMakeLists.txt
+cp ../deb-packages.txt deb-packages.txt
+cp ../build_zip.sh build_zip.sh
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+cd ..
+mv build/tgnews tgnews
+chmod +x tgnews
+zip -r submission.zip src scripts models ratings thirdparty LICENSE NOTICE README.md CMakeLists.txt deb-packages.txt tgnews build_zip.sh
+mv submission.zip ../submission.zip
+cd ..
+rm -rf temp

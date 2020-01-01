9ad007a0ed00a8a92bbbb370b3d99b6bad580b1d
Ilya Gusev <phoenixilya@gmail.com>
2020-01-01 03:03:23 +0300

    ONMT tokenizer

--- src/embedder.cpp
+++ src/embedder.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <cassert>
 
+#include <onmt/Tokenizer.h>
+
 TFastTextEmbedder::TFastTextEmbedder(
     fasttext::FastText& model
     , TFastTextEmbedder::AggregationMode mode

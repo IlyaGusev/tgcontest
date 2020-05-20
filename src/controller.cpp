d247bb857907b9cf4d4e94c537556ad6fcba0df1
hequs <lubanenko@yandex.ru>
2020-05-20 19:57:08 +0300

    parse doc in PUT

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,10 +1,39 @@
 #include "controller.h"
 
+#include "document.h"
+
+#include <tinyxml2/tinyxml2.h>
+
+namespace {
+
+    int ParseTtlHeader(const std::string& value) {
+        static constexpr size_t PREFIX_LEN = std::char_traits<char>::length("max-age=");
+        return std::stoi(value.substr(PREFIX_LEN));
+    }
+
+}
+
 void TController::Put(const drogon::HttpRequestPtr &req, std::function<void(const drogon::HttpResponsePtr&)> &&callback, const std::string& fname) const {
+    const std::string& ttlString = req->getHeader("Cache-Control");
+    if (ttlString.empty()) {
+        throw std::runtime_error("Header Cache-Control is not set");
+    }
+
+    tinyxml2::XMLDocument html;
+    const tinyxml2::XMLError code = html.Parse(req->bodyData(), req->bodyLength());
+    if (code != tinyxml2::XML_SUCCESS) {
+        throw std::runtime_error("Invalid documemt");
+    }
+
+    TDocument doc(html, fname);
+
     Json::Value ret;
     ret["result"] = "put";
-    ret["fname"] = fname;
+    ret["title"] = doc.Title;
+    ret["text"] = doc.Text;
+    ret["ttl"] = ParseTtlHeader(ttlString);
     auto resp = drogon::HttpResponse::newHttpJsonResponse(ret);
+    resp->setStatusCode(drogon::k201Created); // k204NoContent
     callback(resp);
 }
 
@@ -13,5 +42,6 @@ void TController::Delete(const drogon::HttpRequestPtr &req, std::function<void(c
     ret["result"] = "delete";
     ret["fname"] = fname;
     auto resp = drogon::HttpResponse::newHttpJsonResponse(ret);
+    resp->setStatusCode(drogon::k204NoContent); // k404NotFound
     callback(resp);
 }

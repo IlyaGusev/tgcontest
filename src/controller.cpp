8a1376dfc5435d7733127c3d632873c9f1bbad2c
hequs <lubanenko@yandex.ru>
2020-05-22 17:58:32 +0300

    proper return codes

--- src/controller.cpp
+++ src/controller.cpp
@@ -68,6 +68,11 @@ void TController::Put(const drogon::HttpRequestPtr &req, std::function<void(cons
     std::string serializedDoc;
     ToProto(doc, ttl.value()).SerializeToString(&serializedDoc);
 
+    // TODO: possible races while the same fname is provided to multiple queries
+    // TODO: use "value_found" flag and check DB instead of only bloom filter
+    std::string value;
+    const bool mayExist = Context->Db->KeyMayExist(rocksdb::ReadOptions(), fname, &value);
+
     const rocksdb::Status s = Context->Db->Put(rocksdb::WriteOptions(), fname, serializedDoc);
     if (!s.ok()) {
         MakeSimpleResponse(std::move(callback), drogon::k500InternalServerError);
@@ -80,8 +85,9 @@ void TController::Put(const drogon::HttpRequestPtr &req, std::function<void(cons
     ret["title"] = doc.Title;
     ret["text"] = doc.Text;
     ret["ttl"] = ttl.value();
+
     auto resp = drogon::HttpResponse::newHttpJsonResponse(ret);
-    resp->setStatusCode(drogon::k201Created); // k204NoContent
+    resp->setStatusCode(mayExist ? drogon::k204NoContent : drogon::k201Created);
     callback(resp);
 }
 
@@ -90,12 +96,19 @@ void TController::Delete(const drogon::HttpRequestPtr &req, std::function<void(c
         return;
     }
 
-    Json::Value ret;
-    ret["result"] = "delete";
-    ret["fname"] = fname;
-    auto resp = drogon::HttpResponse::newHttpJsonResponse(ret);
-    resp->setStatusCode(drogon::k204NoContent); // k404NotFound
-    callback(resp);
+    // TODO: possible races while the same fname is provided to multiple queries
+    // TODO: use "value_found" flag and check DB instead of only bloom filter
+    std::string value;
+    const bool mayExist = Context->Db->KeyMayExist(rocksdb::ReadOptions(), fname, &value);
+    if (mayExist) {
+        const rocksdb::Status s = Context->Db->Delete(rocksdb::WriteOptions(), fname);
+        if (!s.ok()) {
+            MakeSimpleResponse(std::move(callback), drogon::k500InternalServerError);
+            return;
+        }
+    }
+
+    MakeSimpleResponse(std::move(callback), mayExist ? drogon::k204NoContent : drogon::k404NotFound);
 }
 
 void TController::Get(const drogon::HttpRequestPtr &req, std::function<void(const drogon::HttpResponsePtr&)> &&callback, const std::string& fname) const {
@@ -107,17 +120,16 @@ void TController::Get(const drogon::HttpRequestPtr &req, std::function<void(cons
     const rocksdb::Status s = Context->Db->Get(rocksdb::ReadOptions(), fname, &serializedDoc);
 
     Json::Value ret;
-    ret["result"] = "get";
     ret["fname"] = fname;
+    ret["status"] = s.ok() ? "FOUND" : "NOT FOUND";
 
     if (s.ok()) {
         tg::TDocumentProto doc;
         const auto suc = doc.ParseFromString(serializedDoc);
-        ret["status"] = suc;
+        ret["parsed"] = suc;
         ret["title"] = doc.title();
     }
 
     auto resp = drogon::HttpResponse::newHttpJsonResponse(ret);
-    resp->setStatusCode(drogon::k204NoContent); // k404NotFound
     callback(resp);
 }

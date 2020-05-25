1764460ad1772387d5707661d58d7b6d328dbfd4
Tashkov Ivan <tashkov95@ya.ru>
2020-05-25 19:42:51 +0000

    write and read nasty in dbdocument proto

--- src/db_document.cpp
+++ src/db_document.cpp
@@ -14,6 +14,7 @@ TDbDocument TDbDocument::FromProto(const tg::TDocumentProto& proto) {
     document.Description = proto.text();
     document.Language = proto.language();
     document.Category = proto.category();
+    document.Nasty = proto.nasty();
 
     for (const auto& link : proto.out_links()) {
         document.OutLinks.push_back(link);
@@ -61,6 +62,7 @@ tg::TDocumentProto TDbDocument::ToProto() const {
     proto.set_description(Description);
     proto.set_language(Language);
     proto.set_category(Category);
+    proto.set_nasty(Nasty);
 
     for (const auto& [key, val] : Embeddings) {
         auto* embeddingProto = proto.add_embeddings();

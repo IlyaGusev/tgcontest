8d90fd478406c9812a503f6efa19e1ad8a67b008
Tashkov Ivan <tashkov95@ya.ru>
2020-05-25 19:13:56 +0000

    fix files

--- download_data.sh
+++ download_data.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
 data_train_list=(
-    #"https://data-static.usercontent.dev/DataClusteringSample0107.tar.gz"
-    #"https://data-static.usercontent.dev/DataClusteringSample0817.tar.gz"
-    #"https://data-static.usercontent.dev/DataClusteringSample1821.tar.gz"
-    #"https://data-static.usercontent.dev/DataClusteringSample2225.tar.gz"
-    #"https://data-static.usercontent.dev/DataClusteringDataset.tar.gz"
-    #"https://data-static.usercontent.dev/DataClusteringDataset1209.tar.gz"
-    #"https://data-static.usercontent.dev/DataClusteringDataset0131.tar.gz"
-    #"https://data-static.usercontent.dev/DataClusteringDataset0214.tar.gz"
-    #"https://data-static.usercontent.dev/DataClusteringSample2703.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringSample0107.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringSample0817.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringSample1821.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringSample2225.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringDataset.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringDataset1209.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringDataset0131.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringDataset0214.tar.gz"
+    "https://data-static.usercontent.dev/DataClusteringSample2703.tar.gz"
 )
 
 data_test_list=(
-    #"https://data-static.usercontent.dev/DataClusteringSample0410.tar.gz"
-)
-
-data_val_list=(
-    "https://data-static.usercontent.dev/DataClusteringSample2703.tar.gz"
     "https://data-static.usercontent.dev/DataClusteringSample0410.tar.gz"
-    "https://data-static.usercontent.dev/DataClusteringSample1117.tar.gz"
 )
 
 mkdir data
@@ -32,10 +26,4 @@ mkdir data_test
 for url in ${data_test_list[@]}; do
     echo $url
     wget -qO - $url | tar -xz -C data_test
-done
-
-mkdir data_val
-for url in ${data_val_list[@]}; do
-    echo $url
-    wget -qO - $url | tar -xz -C data_val
 done
